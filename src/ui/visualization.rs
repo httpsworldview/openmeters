3469a324d49ababc10801458834fc12d90a9ffec
Maika Namuo <httpworldview@gmail.com>
2025-10-04 16:34:35 -0700

    fix: expose mods

--- src/ui/visualization.rs
+++ src/ui/visualization.rs
@@ -1,2 +1,5 @@
 pub mod audio_stream;
 pub mod lufs_meter;
+
+pub use audio_stream::AudioStreamSubscription;
+pub use lufs_meter::{LufsMeterState, LufsProcessor, VisualStyle, widget as lufs_meter_widget};
