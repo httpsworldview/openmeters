4f10dfe09acabcbeb71483ce3de44c94352a6f9e
Maika Namuo <httpsworldview@gmail.com>
2025-12-19 01:44:13 -0500

    refactor: remove unused vendored code

--- src/ui/pane_grid/split.rs
+++
@@ -1,6 +0,0 @@
-// This file is adapted from iced_widget v0.13.4 (MIT License).
-/// A divider that splits a region in a [`PaneGrid`] into two different panes.
-///
-/// [`PaneGrid`]: super::PaneGrid
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Split(pub(super) usize);
